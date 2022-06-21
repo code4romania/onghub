e0762a66b1c54595009e873bb6ad5414c4f7d33d
Andrew Radulescu <andrew.radulescu@wearetribus.com>
2022-06-21 13:17:42 +0300

    chore: remove nest/cli

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   await app.listen(3000);
 }
 bootstrap();
