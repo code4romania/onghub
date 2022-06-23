e427c0d3e04aaa9686383f9f397d29761e1e6e94
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-06-23 22:22:03 +0300

    feat: [C4R-146] Mail Catcher setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,8 @@ import { RateLimiterConfigService } from './common/config/rate-limiter-config.se
 import { TypeOrmConfigService } from './common/config/typeorm-config.service';
 import { validate } from './env.validation';
 import { OrganizationModule } from './organization/organization.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
@@ -20,6 +22,29 @@ import { OrganizationModule } from './organization/organization.module';
       useClass: RateLimiterConfigService,
     }),
     OrganizationModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        ignoreTLS: true,
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <no-reply@localhost>',
+      },
+      preview: true,
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   providers: [
     {
