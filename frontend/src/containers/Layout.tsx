47fc9f8aad1c6a8629a89ba66672558027661c94
Dragos Paul Strat <dragos1195@gmail.com>
2022-06-24 12:02:05 +0300

    feat: [C4R-163] more layouting adjustments

--- frontend/src/containers/Layout.tsx
+++ frontend/src/containers/Layout.tsx
@@ -6,13 +6,13 @@ import Menu from '../components/menu/Menu';
 // Compoment that serves as the layout for the app. Everything will be rendered with an header and an side menu.
 const Layout = () => {
   return (
-    <div className="bg-gray-50 w-screen h-screen">
+    <div className="w-screen h-screen max-w-full ">
       <Header />
       <div className="flex p-6">
         <div className="menu">
           <Menu />
         </div>
-        <div className="content w-full pl-6">
+        <div className="content overflow-scroll w-full pl-6">
           <Outlet />
         </div>
       </div>
--- frontend/src/pages/organization/components/OrganizationGeneral.tsx
+++ frontend/src/pages/organization/components/OrganizationGeneral.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const OrganizationGeneral = () => {
   return (
-    <div className="bg-white overflow-hidden shadow rounded-lg">
-      <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
+    <div className="w-full bg-white shadow rounded-lg">
+      <div className=" relative">
+        <div className="p-5 sm:p-10">
+          <span className="font-titilliumBold text-xl text-gray-800">Date generale</span>
+        </div>
+        <div className="w-full border-t border-gray-300" />
+        <div className="p-5 sm:p-10">
+          <span className="font-titilliumBold text-xl text-gray-800">Date generale</span>
+        </div>
+      </div>
     </div>
   );
 };
