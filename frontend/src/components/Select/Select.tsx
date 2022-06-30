dce831eac16c01434fd17dbd7d67edb6df08ec0a
Dragos Paul Strat <dragos1195@gmail.com>
2022-06-30 16:35:30 +0300

    refactor: [C4R-133] add select component

---
+++ frontend/src/components/Select/Select.tsx
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import React, { Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import { classNames } from '../../common/helpers/tailwind.helper';
+
+const Select = (props: {
+  label: string;
+  selected: any;
+  collection: any[];
+  displayedAttribute: string;
+  onChange: any;
+}) => {
+  return (
+    <div className="relative">
+      <Listbox value={props.selected} onChange={props.onChange}>
+        {({ open }) => (
+          <>
+            <Listbox.Label className="block text-sm font-medium text-gray-700 pb-1">
+              {props.label}
+            </Listbox.Label>
+            <div className=" relative">
+              <Listbox.Button className="h-9 bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <span className="block truncate">{props.selected?.name || ''}</span>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                  <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </span>
+              </Listbox.Button>
+
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                  {props.collection.map((item) => (
+                    <Listbox.Option
+                      key={item.id}
+                      className={({ active }) =>
+                        classNames(
+                          active ? 'text-white bg-blue-500' : 'text-gray-900',
+                          'cursor-default select-none relative py-2 pl-3 pr-9',
+                        )
+                      }
+                      value={item}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <span
+                            className={classNames(
+                              selected ? 'font-semibold' : 'font-normal',
+                              'block truncate',
+                            )}
+                          >
+                            {item[props.displayedAttribute]}
+                          </span>
+
+                          {selected ? (
+                            <span
+                              className={classNames(
+                                active ? 'text-white' : 'text-blue-600',
+                                'absolute inset-y-0 right-0 flex items-center pr-4',
+                              )}
+                            >
+                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    </div>
+  );
+};
+
+export default Select;
