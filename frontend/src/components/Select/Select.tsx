35a220060bdbd7fddc09344747b7a5a72db8fcfe
Dragos Paul Strat <dragos1195@gmail.com>
2022-06-30 17:17:16 +0300

    refactor: [C4R-133] add readonly mode for select

--- frontend/src/components/Select/Select.tsx
+++ frontend/src/components/Select/Select.tsx
@@ -10,6 +10,7 @@ const Select = (props: {
   collection: any[];
   displayedAttribute: string;
   selected: any;
+  readonly: boolean;
   onChange: any;
 }) => {
   return (
@@ -20,61 +21,68 @@ const Select = (props: {
             <Listbox.Label className="block text-sm font-medium text-gray-700 pb-1">
               {props.label}
             </Listbox.Label>
-            <div className=" relative">
-              <Listbox.Button className="h-9 bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                <span className="block truncate">{props.selected?.name || ''}</span>
-                <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                  <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                </span>
-              </Listbox.Button>
+            {props.readonly && (
+              <span className="text-normal font-normal text-gray-900">
+                {props.selected[props.displayedAttribute]}
+              </span>
+            )}
+            {!props.readonly && (
+              <div className=" relative">
+                <Listbox.Button className="h-9 bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <span className="block truncate">{props.selected?.name || ''}</span>
+                  <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                    <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  </span>
+                </Listbox.Button>
 
-              <Transition
-                show={open}
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                  {props.collection.map((item) => (
-                    <Listbox.Option
-                      key={item.id}
-                      className={({ active }) =>
-                        classNames(
-                          active ? 'text-white bg-blue-500' : 'text-gray-900',
-                          'cursor-default select-none relative py-2 pl-3 pr-9',
-                        )
-                      }
-                      value={item}
-                    >
-                      {({ selected, active }) => (
-                        <>
-                          <span
-                            className={classNames(
-                              selected ? 'font-semibold' : 'font-normal',
-                              'block truncate',
-                            )}
-                          >
-                            {item[props.displayedAttribute]}
-                          </span>
-
-                          {selected ? (
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                    {props.collection.map((item) => (
+                      <Listbox.Option
+                        key={item.id}
+                        className={({ active }) =>
+                          classNames(
+                            active ? 'text-white bg-blue-500' : 'text-gray-900',
+                            'cursor-default select-none relative py-2 pl-3 pr-9',
+                          )
+                        }
+                        value={item}
+                      >
+                        {({ selected, active }) => (
+                          <>
                             <span
                               className={classNames(
-                                active ? 'text-white' : 'text-blue-600',
-                                'absolute inset-y-0 right-0 flex items-center pr-4',
+                                selected ? 'font-semibold' : 'font-normal',
+                                'block truncate',
                               )}
                             >
-                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              {item[props.displayedAttribute]}
                             </span>
-                          ) : null}
-                        </>
-                      )}
-                    </Listbox.Option>
-                  ))}
-                </Listbox.Options>
-              </Transition>
-            </div>
+
+                            {selected ? (
+                              <span
+                                className={classNames(
+                                  active ? 'text-white' : 'text-blue-600',
+                                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                                )}
+                              >
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            )}
           </>
         )}
       </Listbox>
