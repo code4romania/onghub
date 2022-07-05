3a39ee56ae76f903cec8b0ace439acae12203607
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-07-05 11:31:10 +0300

    refactor(nomenclature): update cities/counties entities and seed data

--- backend/src/shared/entities/county.entity.ts
+++ backend/src/shared/entities/county.entity.ts
@@ -1,8 +1,47 @@
-import { Column, Entity } from 'typeorm';
-import { BaseEntity } from '../../common/base/base-entity.class';
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { City } from './city.entity';
 
 @Entity({ name: '_county' })
-export class County extends BaseEntity {
+export class County {
+  @PrimaryColumn()
+  id: number;
+
   @Column({ type: 'text', name: 'name' })
   name: string;
+
+  @Column({ type: 'text', name: 'abbreviation' })
+  abbreviation: string;
+
+  @OneToMany(() => City, (city) => city.county)
+  cities: City[];
+
+  @Exclude()
+  @DeleteDateColumn({
+    name: 'deleted_on',
+    type: 'timestamp with time zone',
+    select: false,
+  })
+  deletedOn: Date;
+
+  @Index()
+  @CreateDateColumn({ name: 'created_on', type: 'timestamp with time zone' })
+  createdOn: Date;
+
+  @Exclude()
+  @UpdateDateColumn({
+    name: 'updated_on',
+    type: 'timestamp with time zone',
+    select: false,
+  })
+  updatedOn: Date;
 }
