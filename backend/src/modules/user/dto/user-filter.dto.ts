5c1c4d034c3d879975abe2383201e9913f181a29
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-08-09 14:13:58 +0300

    feat(get-users): get employees paginated by organization id

--- backend/src/modules/user/dto/user-filter.dto.ts
+++ backend/src/modules/user/dto/user-filter.dto.ts
@@ -1,14 +1,3 @@
-import { IsEnum, IsOptional } from 'class-validator';
 import { BaseFilterDto } from 'src/common/base/base-filter.dto';
-import { Role } from '../enums/role.enum';
-import { UserStatus } from '../enums/user-status.enum';
 
-export class UserFilterDto extends BaseFilterDto {
-  @IsOptional()
-  @IsEnum(Role)
-  role?: Role;
-
-  @IsOptional()
-  @IsEnum(UserStatus)
-  status?: UserStatus;
-}
+export class UserFilterDto extends BaseFilterDto {}
