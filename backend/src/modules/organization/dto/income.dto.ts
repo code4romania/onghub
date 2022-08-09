ca4da17a4780d3491a37396301924c46561084ba
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-08-09 11:40:12 +0300

    refactor: used a constant for max money values

--- backend/src/modules/organization/dto/income.dto.ts
+++ backend/src/modules/organization/dto/income.dto.ts
@@ -1,38 +1,39 @@
 import { IsNumber, IsPositive, Max } from 'class-validator';
+import { MAX_MONEY } from '../constants/values.constants';
 
 export class Income {
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   membershipFeeIncome: number;
 
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   donationsIncome: number;
 
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   twoPercentIncome: number;
 
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   sponsorshipIncome: number;
 
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   economicActivityIncome: number;
 
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   otherIncome: number;
 
   @IsNumber()
   @IsPositive()
-  @Max(1000000000)
+  @Max(MAX_MONEY)
   financialIncome: number;
 }
