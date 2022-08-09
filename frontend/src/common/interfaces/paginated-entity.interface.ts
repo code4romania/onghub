e6ac6b3fefdc67bb7ec1270a85e00716e552e3d0
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-08-09 17:19:06 +0300

    refactor(users-list): action clolumn based on user status + request correct data based on page init

--- frontend/src/common/interfaces/paginated-entity.interface.ts
+++ frontend/src/common/interfaces/paginated-entity.interface.ts
@@ -1,9 +1,13 @@
+import { OrderDirection } from '../enums/sort-direction.enum';
+
 interface PaginationMeta {
   currentPage: number;
   itemCount: number;
   itermsPerPage: number;
   totalItems: number;
   totalPages: number;
+  orderByColumn: string;
+  orderDirection: OrderDirection;
 }
 
 export interface PaginatedEntity<T> {
--- frontend/src/store/user/Users.slice.ts
+++ frontend/src/store/user/Users.slice.ts
@@ -1,3 +1,4 @@
+import { OrderDirection } from '../../common/enums/sort-direction.enum';
 import { PaginatedEntity } from '../../common/interfaces/paginated-entity.interface';
 import { IUser } from '../../pages/users/interfaces/User.interface';
 
@@ -10,6 +11,8 @@ export const usersSlice = (set: any) => ({
       itermsPerPage: 10,
       totalItems: 0,
       totalPages: 1,
+      orderByColumn: 'name',
+      orderDirection: OrderDirection.ASC,
     },
   },
   setUsers: (users: PaginatedEntity<IUser>) => {
