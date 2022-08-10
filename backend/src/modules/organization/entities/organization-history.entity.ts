03216e823d8bbbae7fa4268f83c04ca1293174e0
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-08-10 14:02:04 +0300

    refactor: changed organization history entity

--- backend/src/modules/organization/entities/organization-history.entity.ts
+++ backend/src/modules/organization/entities/organization-history.entity.ts
@@ -4,12 +4,13 @@ import {
   HistoryEntityInterface,
   HistoryOriginalIdColumn,
 } from '@anchan828/typeorm-history';
-import { Entity } from 'typeorm';
-import { Organization } from '../entities';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { OrganizationStatus } from '../enums/organization-status.enum';
 
 @Entity()
 export class OrganizationHistory
-  extends Organization
+  extends BaseEntity
   implements HistoryEntityInterface
 {
   @HistoryOriginalIdColumn({ name: 'history_original_id' })
@@ -17,4 +18,63 @@ export class OrganizationHistory
 
   @HistoryActionColumn({ name: 'history_action' })
   action: HistoryActionType;
+
+  /*
+   =============================================================
+    Inherited properties
+
+    WARNING: Make sure to keep the properties in sync between
+    the parent entity (Organization) and the History table
+   =============================================================
+   */
+
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({
+    type: 'timestamp with time zone',
+    name: 'synced_on',
+    default: new Date(),
+  })
+  syncedOn: Date;
+
+  @Column({
+    type: 'enum',
+    enum: OrganizationStatus,
+    name: 'status',
+    default: OrganizationStatus.PENDING,
+  })
+  status: OrganizationStatus;
+
+  @Exclude()
+  @Column({
+    type: 'integer',
+    nullable: true,
+    name: 'organization_general_id',
+  })
+  organizationGeneralId: number;
+
+  @Exclude()
+  @Column({
+    type: 'integer',
+    nullable: true,
+    name: 'organization_activity_id',
+  })
+  organizationActivityId: number;
+
+  @Exclude()
+  @Column({
+    type: 'integer',
+    nullable: true,
+    name: 'organization_legal_id',
+  })
+  organizationLegalId: number;
+
+  @Exclude()
+  @Column({
+    type: 'integer',
+    nullable: true,
+    name: 'organization_report_id',
+  })
+  organizationReportId: number;
 }
