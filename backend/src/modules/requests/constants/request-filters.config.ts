77ae8343ebedb141464845fb8dd1ecfee949bf67
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-08-11 18:32:21 +0300

    chore: update requests config

--- backend/src/modules/requests/constants/request-filters.config.ts
+++ backend/src/modules/requests/constants/request-filters.config.ts
@@ -1,17 +1,10 @@
 import { OrderDirection } from 'src/common/enums/order-direction.enum';
 
 export const REQUEST_FILTER_CONFIG = {
-  selectColumns: [
-    'name',
-    'email',
-    'phone',
-    'status',
-    'organization',
-    'createdOn',
-  ],
-  searchableColumns: ['name'],
+  selectColumns: ['id', 'name', 'email', 'phone', 'organization', 'createdOn'],
+  searchableColumns: ['name', 'organization.organizationGeneral.name'],
   defaultSortBy: 'createdOn',
   defaultOrderDirection: OrderDirection.ASC,
-  relations: ['organization'],
+  relations: ['organization', 'organization.organizationGeneral'],
   rangeColumn: 'createdOn',
 };
