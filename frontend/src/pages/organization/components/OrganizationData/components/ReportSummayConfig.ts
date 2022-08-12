7ada498c41c0873067d5382948c4bf2293192acb
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-08-12 17:28:21 +0300

    fix: initial changes

--- frontend/src/pages/organization/components/OrganizationData/components/ReportSummayConfig.ts
+++ frontend/src/pages/organization/components/OrganizationData/components/ReportSummayConfig.ts
@@ -1,9 +1,19 @@
 import InputFieldHttpAddon from '../../../../../components/InputField/components/InputFieldHttpAddon';
+import { MAX_REPORT } from '../../../constants/values.constants';
 
 export const ReportSummaryConfig: Record<string, any> = {
   numberOfVolunteers: {
     key: 'numberOfVolunteers',
-    rules: {},
+    rules: {
+      min: {
+        value: 0,
+        message: 'Number of volunteers cannot have negative values.',
+      },
+      max: {
+        value: MAX_REPORT,
+        message: 'Number of volunteers cannot exceed 6 digits.',
+      },
+    },
     config: {
       type: 'number',
       label: 'Numar voluntari',
@@ -13,7 +23,16 @@ export const ReportSummaryConfig: Record<string, any> = {
   },
   numberOfContractors: {
     key: 'numberOfContractors',
-    rules: {},
+    rules: {
+      min: {
+        value: 0,
+        message: 'Number of contractors cannot have negative values.',
+      },
+      max: {
+        value: MAX_REPORT,
+        message: 'Number of contractors cannot exceed 6 digits.',
+      },
+    },
     config: {
       type: 'number',
       label: 'Numar contractori',
--- frontend/src/pages/organization/constants/values.constants.tsx
+++ frontend/src/pages/organization/constants/values.constants.tsx
@@ -1,1 +1,2 @@
 export const MAX_MONEY = 1000000000;
+export const MAX_REPORT = 999999;
