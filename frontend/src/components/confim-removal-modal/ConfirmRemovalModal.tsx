b8b73931d0a5b6beb2489653553e71c5206e7db4
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-08-16 14:25:00 +0300

    refactor(account): promote confirmation removal modal to common component

---
+++ frontend/src/components/confim-removal-modal/ConfirmRemovalModal.tsx
@@ -0,0 +1,79 @@
+import React, { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+interface ConfirmRemovalModalProps {
+  title: string;
+  description: string;
+  closeBtnLabel: string;
+  confirmBtnLabel: string;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const ConfirmRemovalModal = ({
+  title,
+  description,
+  closeBtnLabel,
+  confirmBtnLabel,
+  onClose,
+  onConfirm,
+}: ConfirmRemovalModalProps) => {
+  return (
+    <Transition.Root show={true} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-10">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title as="h3" className="text-xl leading-6 font-bold text-gray-900">
+                      {title}
+                    </Dialog.Title>
+                    <div className="mt-6">
+                      <p className="text-base text-gray-900 font-normal">{description}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6 sm:flex sm:flex-row-reverse">
+                  <button type="button" className="red-button" onClick={onConfirm}>
+                    {confirmBtnLabel}
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                    onClick={onClose}
+                  >
+                    {closeBtnLabel}
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default ConfirmRemovalModal;
