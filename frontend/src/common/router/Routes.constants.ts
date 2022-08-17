99cb7812d42889c472908e99355fc6fe5621481b
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-08-17 16:50:22 +0300

    chore: update admin menu items

--- frontend/src/common/router/Routes.constants.ts
+++ frontend/src/common/router/Routes.constants.ts
@@ -22,8 +22,6 @@ export const ADMIN_ROUTES = [
 
 export const SUPER_ADMIN_ROUTES = [
   { id: 0, name: 'Dashboard', href: '', icon: TemplateIcon },
-  { id: 1, name: 'Organizatia mea', href: 'organization', icon: SunIcon },
   { id: 2, name: 'Utilizatori', href: 'users', icon: UserGroupIcon },
-  { id: 3, name: 'Aplicatiile mele', href: 'apps', icon: ViewGridAddIcon },
   { id: 4, name: 'Toate aplicatiile', href: 'store', icon: CollectionIcon },
 ];
