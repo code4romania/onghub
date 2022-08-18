b46642a542c04780648063e72e404cabe6d5f566
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-08-18 09:53:27 +0300

    refactor: add header to login screen

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Auth, Hub } from 'aws-amplify';
+import Header from '../../components/Header/Header';
 
 const Login = () => {
   useEffect(() => {
@@ -38,18 +39,21 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <button
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            onClick={(e) => Auth.federatedSignIn()}
-          >
-            Sign in HostedUI
-          </button>
+    <>
+      <Header />
+      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="mt-8 sm:mx-auto sm:w-full h-screen justify-center items-center sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <button
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={(e) => Auth.federatedSignIn()}
+            >
+              Sign in HostedUI
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
