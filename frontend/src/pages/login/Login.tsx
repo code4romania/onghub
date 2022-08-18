b40b541fc34d0aea641ecc9490b7e869d30ff783
Dragos-Paul Strat <dragos1195@gmail.com>
2022-08-18 11:06:19 +0300

    refactor: [C4R-391] add modal and login page

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -41,18 +41,7 @@ const Login = () => {
   return (
     <>
       <Header />
-      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="mt-8 sm:mx-auto sm:w-full h-screen justify-center items-center sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <button
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              onClick={(e) => Auth.federatedSignIn()}
-            >
-              Sign in HostedUI
-            </button>
-          </div>
-        </div>
-      </div>
+      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gradient-to-l from-yellow-500 to-yellow-800 w-screen h-screen"></div>
     </>
   );
 };
--- frontend/src/pages/requests/components/Request.tsx
+++ frontend/src/pages/requests/components/Request.tsx
@@ -5,6 +5,11 @@ import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { classNames } from '../../../common/helpers/tailwind.helper';
 import { useErrorToast, useSuccessToast } from '../../../common/hooks/useToast';
 import { IPageTab } from '../../../common/interfaces/tabs.interface';
+import ConfirmRemovalModal, {
+  ConfirmRemovalModalProps,
+} from '../../../components/confim-removal-modal/ConfirmRemovalModal';
+import ContentWrapper from '../../../components/content-wrapper/ContentWrapper';
+import { Loading } from '../../../components/loading/Loading';
 import { useCountiesQuery } from '../../../services/nomenclature/Nomenclature.queries';
 import {
   useApproveRequestMutation,
@@ -12,7 +17,8 @@ import {
   useRequest,
 } from '../../../services/request/Request.queries';
 import { ORGANIZATION_TABS } from '../../organization/constants/Tabs.constants';
-import { OrganizationStatus } from '../../organization/enums/OrganizationStatus.enum';
+import { APPROVE_MODAL, REJECT_MODAL } from '../constants/Request.modals';
+import { RequestStatus } from '../enum/RequestStatus.enum';
 
 const Request = () => {
   const { id } = useParams();
@@ -20,6 +26,8 @@ const Request = () => {
   const location = useLocation();
   const params = useParams();
   const [selectedTab, setSelectedTab] = useState(0);
+  // const [modalConfig, setModalConfig] = useState({} as Partial<ConfirmRemovalModalProps>);
+  // const [isModalOpen, setModalOpen] = useState(false);
 
   // TODO: Load nomenclature data on app init
   useCountiesQuery();
@@ -28,8 +36,16 @@ const Request = () => {
   const { data: request, error, refetch } = useRequest(params.id ? params?.id : '');
 
   // apporve & reject
-  const { mutateAsync: approveMutate, error: approveError } = useApproveRequestMutation();
-  const { mutateAsync: rejectMutate, error: rejectError } = useRejectRequestMutation();
+  const {
+    mutateAsync: approveMutate,
+    error: approveError,
+    isLoading: approveLoading,
+  } = useApproveRequestMutation();
+  const {
+    mutateAsync: rejectMutate,
+    error: rejectError,
+    isLoading: rejectLoading,
+  } = useRejectRequestMutation();
 
   useEffect(() => {
     const found: IPageTab | undefined = ORGANIZATION_TABS.find(
@@ -71,55 +87,62 @@ const Request = () => {
     });
   };
 
+  if (approveLoading || rejectLoading) {
+    return <Loading />;
+  }
+
   return (
     <div>
-      <p className="text-gray-800 font-titilliumBold text-3xl">
-        {request?.organization?.organizationGeneral?.name}
-      </p>
-      <p className="text-gray-400 pt-6">
-        Administrează de aici profilul tău de organizație pentru a putea accesa aplicațiile
-        disponibile.
-      </p>
-      {request?.organization?.status === OrganizationStatus.PENDING && (
-        <div className="w-full bg-white shadow rounded-lg py-5 px-10 flex justify-between items-center flex-col sm:flex-row mt-4">
-          <div className="flex gap-4">
-            <ExclamationIcon className="text-orange h-7 w-7" />
-            <p className="text-gray-800 font-titilliumBold rounded-md  text-xl hover:bg-green-tab lg:whitespace-nowrap">
-              Datele de mai jos corespund unei solicitari in curs de aprobare.
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <button type="button" className="red-button gap-2" onClick={onReject}>
-              <XIcon className="w-5 h-5" /> Respinge
-            </button>
-            <button type="button" className="save-button gap-2" onClick={onApprove}>
-              <CheckIcon className="w-5 h-5" /> Aproba
-            </button>
-          </div>
-        </div>
-      )}
-      <div className="pb-6 flex">
-        <nav
-          className="flex  pt-6 flex-col space-y-4 sm:space-y-0 sm:gap-x-4 sm:gap-y-4 flex-wrap lg:flex-row cursor-pointer select-none"
-          aria-label="Tabs"
-        >
-          {ORGANIZATION_TABS.map((tab) => (
-            <a
-              key={tab.name}
-              onClick={() => onTabClick(tab)}
-              className={classNames(
-                selectedTab === tab.id
-                  ? 'bg-green-tab text-gray-800 font-titilliumBold'
-                  : 'font-titilliumSemiBold',
-                'text-gray-700 rounded-md  text-xl px-8 py-2 hover:bg-green-tab lg:whitespace-nowrap',
-              )}
+      <ContentWrapper
+        title={request?.organization.organizationGeneral.name || ''}
+        subtitle=" Administrează de aici profilul tău de organizație pentru a putea accesa aplicațiile
+    disponibile."
+        goBack={() => navigate('/requests')}
+      >
+        <div>
+          {request?.status === RequestStatus.PENDING && (
+            <div className="w-full bg-white shadow rounded-lg py-5 px-10 flex justify-between items-center flex-col sm:flex-row mt-4">
+              <div className="flex gap-4">
+                <ExclamationIcon className="text-orange h-7 w-7" />
+                <p className="text-gray-800 font-titilliumBold rounded-md  text-xl hover:bg-green-tab lg:whitespace-nowrap">
+                  Datele de mai jos corespund unei solicitari in curs de aprobare.
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <button type="button" className="red-button gap-2" onClick={onReject}>
+                  <XIcon className="w-5 h-5" /> Respinge
+                </button>
+                <button type="button" className="save-button gap-2" onClick={onApprove}>
+                  <CheckIcon className="w-5 h-5" /> Aproba
+                </button>
+              </div>
+            </div>
+          )}
+          <div className="pb-6 flex">
+            <nav
+              className="flex  pt-6 flex-col space-y-4 sm:space-y-0 sm:gap-x-4 sm:gap-y-4 flex-wrap lg:flex-row cursor-pointer select-none"
+              aria-label="Tabs"
             >
-              {tab.name}
-            </a>
-          ))}
-        </nav>
-      </div>
-      <Outlet />
+              {ORGANIZATION_TABS.map((tab) => (
+                <a
+                  key={tab.name}
+                  onClick={() => onTabClick(tab)}
+                  className={classNames(
+                    selectedTab === tab.id
+                      ? 'bg-green-tab text-gray-800 font-titilliumBold'
+                      : 'font-titilliumSemiBold',
+                    'text-gray-700 rounded-md  text-xl px-8 py-2 hover:bg-green-tab lg:whitespace-nowrap',
+                  )}
+                >
+                  {tab.name}
+                </a>
+              ))}
+            </nav>
+          </div>
+          <Outlet />
+        </div>
+      </ContentWrapper>
+      {/* {isModalOpen && <ConfirmRemovalModal {...modalConfig} onClose={() => setModalOpen(false)} onConfirm={} />} */}
     </div>
   );
 };
