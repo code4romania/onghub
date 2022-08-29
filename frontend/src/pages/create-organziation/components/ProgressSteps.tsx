dffdeb8c4a1f6475ac97e6add579fb062796a6da
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-08-29 13:58:06 +0300

    fix: fixed steps being skippable on create org

--- frontend/src/pages/create-organziation/components/ProgressSteps.tsx
+++ frontend/src/pages/create-organziation/components/ProgressSteps.tsx
@@ -85,12 +85,8 @@ export default function ProgressSteps({ disabled }: { disabled: boolean }) {
         role="list"
         className="space-y-4 md:flex md:space-y-0 md:space-x-8 bg-white rounded-lg shadow p-5 sm:p-10  m-1"
       >
-        {steps.map((step, stepIndex) => (
-          <li
-            key={step.name}
-            className="md:flex-1 cursor-pointer"
-            onClick={() => progress(step, stepIndex)}
-          >
+        {steps.map((step) => (
+          <li key={step.name} className="md:flex-1 cursor-pointer">
             {step.status === PROGRESS_STEP_TYPE.COMPLETE ? (
               <a className="group pl-4 py-2 flex flex-col border-l-4 border-indigo-600 hover:border-indigo-800 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4">
                 <span className="text-xs text-indigo-600 font-semibold tracking-wide uppercase group-hover:text-indigo-800">
