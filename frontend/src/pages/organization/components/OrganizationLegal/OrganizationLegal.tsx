a40e0d3b2f7077f0a3b0a88aa67db52740683dc9
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-09-06 11:29:44 +0300

    finished with create-org

--- frontend/src/pages/organization/components/OrganizationLegal/OrganizationLegal.tsx
+++ frontend/src/pages/organization/components/OrganizationLegal/OrganizationLegal.tsx
@@ -368,7 +368,7 @@ const OrganizationLegal = () => {
                       htmlFor="uploadPhoto"
                       className="w-32 cursor-pointer bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                      {t('statute_upload', { ns: 'common' })}
+                      {t('statute_upload')}
                     </label>
                     <input
                       className="h-0 w-0"
