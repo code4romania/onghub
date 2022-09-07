13671f456963b2db601ee238c6ab27e54a80d7fc
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-09-07 18:07:45 +0300

    started on apps

--- frontend/src/pages/application/constants/ApplicationTabs.tsx
+++ frontend/src/pages/application/constants/ApplicationTabs.tsx
@@ -1,7 +1,14 @@
 import { IPageTab } from '../../../common/interfaces/tabs.interface';
+import i18n from '../../../common/config/i18n';
+
+const translations = {
+  description: i18n.t('app:tabs.description'),
+  list: i18n.t('app:tabs.list'),
+  requests: i18n.t('app:tabs.requests'),
+};
 
 export const APPLICATION_TABS: IPageTab[] = [
-  { id: 0, name: 'Descriere', href: '' },
-  { id: 1, name: 'Lista ONG-uri', href: 'installs' },
-  { id: 2, name: 'Solicitari', href: 'requests' },
+  { id: 0, name: translations.description, href: '' },
+  { id: 1, name: translations.list, href: 'installs' },
+  { id: 2, name: translations.requests, href: 'requests' },
 ];
