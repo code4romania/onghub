67200c77a98c088c97075f915cffdee24e3df1b5
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-09-07 17:28:52 +0300

    fix(application-request): set correct relation for application request

--- backend/src/modules/application/entities/application-request.entity.ts
+++ backend/src/modules/application/entities/application-request.entity.ts
@@ -1,5 +1,5 @@
 import { Application } from 'src/modules/application/entities/application.entity';
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 import { Request } from '../../../shared/entities/request.entity';
 
 @Entity({ name: 'application_request' })
@@ -7,7 +7,7 @@ export class ApplicationRequest extends Request {
   @Column({ type: 'integer', nullable: true, name: 'application_id' })
   applicationId: number;
 
-  @OneToOne((type) => Application)
+  @ManyToOne((type) => Application)
   @JoinColumn({ name: 'application_id' })
   application: Application;
 }
