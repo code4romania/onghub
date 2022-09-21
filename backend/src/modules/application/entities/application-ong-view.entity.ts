9305b27e2dacdc5855fa42b5b246901771459bae
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-09-21 11:19:01 +0300

    fix: application ong join with user_ong_application

--- backend/src/modules/application/entities/application-ong-view.entity.ts
+++ backend/src/modules/application/entities/application-ong-view.entity.ts
@@ -14,7 +14,7 @@ import { OngApplicationStatus } from '../enums/ong-application-status.enum';
   LEFT JOIN "ong_application" "ong_application" ON "ong_application".organization_id = "organization".id
   LEFT JOIN "application" "application" ON "application".id = "ong_application".application_id
   LEFT JOIN "organization_general" "organization_general" ON "organization_general".id = "organization".organization_general_id
-  LEFT JOIN "user_ong_application" "user_ong_application" ON "user_ong_application".application_id = "ong_application".id
+  LEFT JOIN "user_ong_application" "user_ong_application" ON "user_ong_application".ong_application_id = "ong_application".id
   GROUP BY "organization".id, "organization_general".id, "ong_application".id, "application".id`,
 })
 export class ApplicationOngView {
