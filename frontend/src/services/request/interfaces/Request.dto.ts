dcb385299bef22a235e2ed17a5041aec50237395
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-10-05 18:09:34 +0300

    chore: remove unused interfaces

--- frontend/src/services/request/interfaces/Request.dto.ts
+++ frontend/src/services/request/interfaces/Request.dto.ts
@@ -1,15 +1,9 @@
-import { ICreateOrganizationPayload } from '../../../pages/create-organziation/interfaces/CreateOrganization.interface';
 import { IOrganizationGeneral } from '../../../pages/organization/interfaces/OrganizationGeneral.interface';
 
 export interface CreateRequestAdminDTO {
   name: string;
   email: string;
   phone: string;
-}
-
-export interface CreateOrganizationRequestDTO {
-  admin: CreateRequestAdminDTO;
-  // organization: ICreateOrganizationPayload;
 }
 
 export interface ValidateCreateOrganizationRequest {
