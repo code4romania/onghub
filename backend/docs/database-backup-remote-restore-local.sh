302543be0dcb3700c799878c4cfcb48cbbb362f7
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-10-12 16:13:46 +0300

    refactor: updated echo texts for clarity

--- backend/docs/database-backup-remote-restore-local.sh
+++ backend/docs/database-backup-remote-restore-local.sh
@@ -4,15 +4,16 @@
 # chmod 755 your_script.sh
 # ./your_script.sh
 
-echo What is your remote database?
+echo Enter your remote database URL
 read REMOTE_DATABASE
-echo What is your username?
+echo Enter your database username
 read USER_NAME
 echo Name your file
 read FILE_NAME
 # This command will backup the remote database
+# You will be prompted to enter the password
 pg_dump -h $REMOTE_DATABASE -U $USER_NAME -f $FILE_NAME.sql
-echo What is your docker container id?
+echo Enter your database container id (use 'docker ps' to find)
 read CONTAINER_ID
 # This command will restore the backup inside your chosen docker container
 cat $FILE_NAME.sql | docker exec -i $CONTAINER_ID psql -U $USER_NAME
