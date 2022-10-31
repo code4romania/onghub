ea8c350f8664cc428493c810e090ad1bcb81189f
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-10-31 10:12:29 +0200

    refactor: [C4R-817] removed import from module

--- backend/src/modules/_publicAPI/public-api.module.ts
+++ backend/src/modules/_publicAPI/public-api.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationModule } from '../application/application.module';
-import { PublicApiSendMailService } from './public-api-send-mail.service';
 import { PublicAPIController } from './public-api.controller';
 import { PublicKeysManager } from './public-keys-manager.service';
 import { PublicKeys } from './public-keys.entity';
@@ -9,7 +8,7 @@ import { PublicKeys } from './public-keys.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([PublicKeys]), ApplicationModule],
   controllers: [PublicAPIController],
-  providers: [PublicKeysManager, PublicApiSendMailService],
+  providers: [PublicKeysManager],
   exports: [],
 })
 export class PublicAPIModule {}
