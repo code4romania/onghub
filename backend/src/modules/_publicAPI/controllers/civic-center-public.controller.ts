9a1f619a09771e40cf23e59629b844c25ea8cbe3
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-12-15 10:56:36 +0200

    fix: [C4R-1351] moved the feedback cretion to public API

--- backend/src/modules/_publicAPI/controllers/civic-center-public.controller.ts
+++ backend/src/modules/_publicAPI/controllers/civic-center-public.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiBearerAuth, ApiParam } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiParam } from '@nestjs/swagger';
 import { Public } from 'src/common/decorators/public.decorator';
 import { Pagination } from 'src/common/interfaces/pagination';
+import { CreateFeedbackDto } from 'src/modules/civic-center-service/dto/create-feedback.dto';
+import { Feedback } from 'src/modules/civic-center-service/entities/feedback.entity';
+import { FeedbackService } from 'src/modules/civic-center-service/services/feedback.service';
 import { OrganizationWithServices } from 'src/modules/organization/interfaces/OrganizationWithServices.interface';
 import { CivicCenterServiceSearchFilterDto } from '../../civic-center-service/dto/civic-center-service-search-filter.dto';
 import { CivicCenterService } from '../../civic-center-service/entities/civic-center-service.entity';
@@ -15,6 +18,7 @@ import { CivicCenterPublicService } from '../services/civic-center-public.servic
 export class CivicCenterPublicController {
   constructor(
     private readonly civicCenterServicePublic: CivicCenterPublicService,
+    private readonly feedbackService: FeedbackService,
   ) {}
 
   @Public()
@@ -50,5 +54,17 @@ export class CivicCenterPublicController {
     CivicCenterService & { organizationId: number; organizationName: string }
   > {
     return this.civicCenterServicePublic.get(id);
+  }
+
+  @Public()
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({ type: CreateFeedbackDto })
+  @Post('/service/:id/feedback')
+  async create(
+    @Param('id') id: string,
+    @Body() body: CreateFeedbackDto,
+  ): Promise<Feedback> {
+    console.log(body);
+    return this.feedbackService.create(+id, body);
   }
 }
