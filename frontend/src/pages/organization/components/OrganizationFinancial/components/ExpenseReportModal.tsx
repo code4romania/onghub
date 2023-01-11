837f3e5c75b8c47a385f1270558d672a6e56d162
Andreea Pascu <andreeamaria10p@gmail.com>
2023-01-11 17:11:46 +0200

    C4R-1549 - Fixing scroll bars

--- frontend/src/pages/organization/components/OrganizationFinancial/components/ExpenseReportModal.tsx
+++ frontend/src/pages/organization/components/OrganizationFinancial/components/ExpenseReportModal.tsx
@@ -124,7 +124,7 @@ const ExpenseReportModal = ({
                   <ExternalLinkIcon className="w-4 h-4 ml-1 text-gray-400 cursor-pointer" />
                   {t('modal.anaf')}
                 </a>
-                <form className="overflow-scroll">
+                <form className="sm:overflow-y-auto overflow-scroll">
                   <table className="min-w-full divide-y divide-gray-300 mt-8">
                     <thead className="bg-gray-100">
                       <tr>
