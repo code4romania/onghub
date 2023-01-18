1d27dd995f406c939aa0e7cc4ecadf559e47953a
Andrew Radulescu <andrew.radulescu@wearetribus.com>
2023-01-18 15:58:51 +0200

    fix: WIP - email STARTTLS

--- backend/src/common/config/email-config.service.ts
+++ backend/src/common/config/email-config.service.ts
@@ -8,11 +8,12 @@ export class EmailConfigService {
   constructor(private readonly configService: ConfigService) {}
 
   createMailerOptions(): MailerOptions {
+    console.log(this.configService.get('MAIL_PASS'));
     return {
       transport: {
         host: this.configService.get('MAIL_HOST'),
         port: +this.configService.get('MAIL_PORT'),
-        // ignoreTLS: true, // - if this is true and secure is false then TLS is not used even if the server supports STARTTLS extension
+        ignoreTLS: false, // - if this is true and secure is false then TLS is not used even if the server supports STARTTLS extension
         secure: false, // – if true the connection will use TLS when connecting to server. If false (the default) then TLS is used if server supports the STARTTLS extension. In most cases set this value to true if you are connecting to port 465. For port 587 or 25 keep it false
         auth: {
           user: this.configService.get('MAIL_USER'),
