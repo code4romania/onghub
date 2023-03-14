b418620e343749ae57fe25cd7886e8897995b173
Andrei Ioniță <hi@andrei.io>
2023-03-14 13:49:07 +0000

    infra: fix lambda log arn (#301)

--- terraform/data.tf
+++ terraform/data.tf
@@ -68,7 +68,7 @@ data "aws_iam_policy_document" "lambda_logging_policy" {
       "logs:PutLogEvents"
     ]
 
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:/aws/lambda/*"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"]
   }
 }
 
