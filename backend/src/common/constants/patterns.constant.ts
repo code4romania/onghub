65b059d8744929c3aa7c7b0e8b8a0d2258542fb8
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-10-29 15:32:28 +0200

    fix(C4R-340): update regex for create/update organization general dto to support '.' for raf number

--- backend/src/common/constants/patterns.constant.ts
+++ backend/src/common/constants/patterns.constant.ts
@@ -5,5 +5,5 @@ export const REGEX = {
   NAME: /^(?!.*[ ]{2})[a-zA-Z\s-ăîâșțĂÎÂȘȚ]*$/,
   LINK: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.?[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https:\/\/(?:www\.|(?!www))|[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})|^$/,
   CUI: /^(RO|)?[\d]*$/,
-  RAF: /^[a-zA-Z\d/]*$/,
+  RAF: /^[a-zA-Z\d/.]*$/,
 };
