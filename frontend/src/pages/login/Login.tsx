ec0fc2590a35d1bbf1dd01ffb377fe5eba8b68ec
Andreea Pascu <andreeamaria10p@gmail.com>
2023-12-15 15:06:56 +0200

    feat: created login/landing page

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react';
 import { Hub } from 'aws-amplify';
 import Header from '../../components/Header/Header';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+  const { t } = useTranslation(['login']);
+  const navigate = useNavigate();
+
   useEffect(() => {
     const listener = Hub.listen('auth', ({ payload: { event, data } }) => {
       switch (event) {
@@ -42,8 +48,43 @@ const Login = () => {
 
   return (
     <>
-      <Header />
-      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gradient-to-l from-yellow-500 to-yellow-800 w-screen h-screen"></div>
+      <div className="flex flex-col items-center bg-loginBackground bg-cover w-screen min-h-screen h-fit gap-6">
+        <div className="w-full">
+          <Header />
+        </div>
+        <div className="flex flex-col items-center p-4 gap-6">
+          <div className="pt-10 sm:pt-24 font-titilliumSemiBold text-3xl md:text-5xl text-center">
+            {t('title')}
+          </div>
+          <div className="font-titilliumSemiBold text-lg md:text-xl max-w-3xl text-center pb-10 sm:pb-14">
+            {t('description')}
+          </div>
+          <div className="w-full max-w-4xl bg-white shadow rounded-lg px-4 sm:px-10 py-4 sm:py-[58px] flex sm:grid gap-6 sm:gap-12 sm:grid-cols-2 flex-col-reverse">
+            <div className="flex flex-col gap-5">
+              <div className="font-titilliumBold text-2xl md:text-[2rem]">{t('about.title')}</div>
+              <div className="font-titillium">
+                {t('about.description')}
+                <ul className="list-disc pl-2 font-titillium">
+                  {t('about.bullet_1') && <li className="list-inside">{t('about.bullet_1')}</li>}
+                  {t('about.bullet_2') && <li className="list-inside">{t('about.bullet_2')}</li>}
+                  {t('about.bullet_3') && <li className="list-inside">{t('about.bullet_3')}</li>}
+                </ul>
+              </div>
+
+              <button
+                aria-label={t('about.start_form')}
+                className="bg-yellow-600 hover:bg-yellow-700 sm:text-base px-6 py-3 text-sm shadow rounded-md text-black font-titilliumSemiBold w-fit"
+                onClick={(event) => {
+                  navigate('/new/account');
+                }}
+              >
+                {t('about.start_form')}
+              </button>
+            </div>
+            <div className="bg-clock bg-no-repeat bg-contain w-full h-40 sm:h-full bg-center"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
