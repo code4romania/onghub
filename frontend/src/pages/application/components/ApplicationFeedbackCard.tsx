fb0980aef4ceda7a14b069adbf4c57b5129603b8
luciatugui <lucia.tugui@wearetribus.com>
2024-07-11 10:22:33 +0300

    feat: allow rich text formatting for extended description

--- frontend/src/pages/application/components/ApplicationFeedbackCard.tsx
+++ frontend/src/pages/application/components/ApplicationFeedbackCard.tsx
@@ -5,6 +5,7 @@ interface ApplicationFeedbackCardProps {
   description: string;
   icon?: React.ReactNode;
   actions?: React.ReactNode;
+  descriptionIsHtml?: boolean;
 }
 
 const ApplicationFeedbackCard = ({
@@ -12,9 +13,10 @@ const ApplicationFeedbackCard = ({
   title,
   description,
   actions,
+  descriptionIsHtml,
 }: ApplicationFeedbackCardProps) => {
   return (
-    <div className="w-full h-full bg-white shadow rounded-lg">
+    <div className="w-full h-full shadow rounded-lg">
       <div className="py-5 lg:px-10 px-5 flex gap-2 items-center">
         {icon}
         <span className="font-titilliumBold sm:text-lg lg:text-xl text-md text-gray-800">
@@ -23,7 +25,15 @@ const ApplicationFeedbackCard = ({
       </div>
       <div className="w-full border-t border-gray-300" />
       <div className="lg:p-8 p-5 flex flex-col gap-4">
-        <p className="break-word sm:text-sm lg:text-base text-xs">{description}</p>
+        {descriptionIsHtml ? (
+          <p
+            className="richtext_html break-word sm:text-sm lg:text-base text-xs"
+            dangerouslySetInnerHTML={{ __html: description || '' }}
+          />
+        ) : (
+          <p className="break-word sm:text-sm lg:text-base text-xs">{description}</p>
+        )}
+
         <div>{actions}</div>
       </div>
     </div>
