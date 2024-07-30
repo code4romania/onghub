dde46f7ad282e2fdb094f106e0ba787711220691
Dragos-Paul Strat <dragos1195@gmail.com>
2024-07-30 17:35:13 +0300

    fix: [520] add sectors

---
+++ backend/src/migrations/seed/sectors.seed.ts
@@ -0,0 +1,32 @@
+export const SECTORS = [
+  {
+    id: 13930,
+    countyId: 11,
+    name: 'Sector 1',
+  },
+  {
+    id: 13931,
+    countyId: 11,
+    name: 'Sector 2',
+  },
+  {
+    id: 13932,
+    countyId: 11,
+    name: 'Sector 3',
+  },
+  {
+    id: 13933,
+    countyId: 11,
+    name: 'Sector 4',
+  },
+  {
+    id: 13934,
+    countyId: 11,
+    name: 'Sector 5',
+  },
+  {
+    id: 13935,
+    countyId: 11,
+    name: 'Sector 6',
+  },
+];
