31b6a7be6d3689fcd8135068804f1c07e1050576
luciatugui <lucia.tugui@wearetribus.com>
2024-08-27 15:38:45 +0300

    fix: actionLink on banner & superAdmin app dashboard cards

--- frontend/src/components/warning-banner/WarningBanner.tsx
+++ frontend/src/components/warning-banner/WarningBanner.tsx
@@ -3,21 +3,24 @@ import { ExclamationTriangleIcon } from '@heroicons/react/24/solid';
 
 interface WarningBannerProps {
   text: string;
-  actionText: string;
+  actionText?: string;
+  actionLink?: string;
 }
-const WarningBanner = ({ text, actionText }: WarningBannerProps) => {
+const WarningBanner = ({ text, actionText, actionLink }: WarningBannerProps) => {
   return (
     <div className="bg-amber-50 flex flex-row items-center gap-2">
       <div className="bg-yellow-700 w-1 self-stretch"></div>
       <ExclamationTriangleIcon width={16} height={16} fill="rgb(251 192 45)" />
       <p className="py-3 text-amber-800 text-xs">
         {text}{' '}
-        <a
-          href="/organization/financial"
-          className="text-amber-800 hover:text-amber-700 underline underline-offset-2"
-        >
-          {actionText}
-        </a>
+        {actionText && (
+          <a
+            href={actionLink}
+            className="text-amber-800 hover:text-amber-700 underline underline-offset-2"
+          >
+            {actionText}
+          </a>
+        )}
       </p>
     </div>
   );
